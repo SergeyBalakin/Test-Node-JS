ac39901d5e2560f9de2ac1fba4736fda5df37115
SergeyBalakin <Balakinsergey83@gmail.com>
2020-04-18 03:20:47 +0300

    разбираюсь в Node JS

---
+++ demo/path.js
@@ -0,0 +1,7 @@
+const path = require("path");
+
+console.log("Название файла:", path.basename(__filename));
+console.log("Название директории:", path.dirname(__filename));
+console.log("Расширение файла:", path.extname(__filename));
+console.log("Parse:", path.parse(__filename).name);
+console.log(path.join(__dirname, "server", "index.html"));
